7d625b7fd001816139e72e9e3e916e70bc64a639
GeloCraft <115651305+gelocraft@users.noreply.github.com>
2025-05-24 15:09:29 +0800

    wip(v0.1.0-dev): 05/24/25 15:07:53 (#1)
    * wip: update package json
    
    * wip: 05/24/25 14:48:40
    
    * wip: 05/24/25 15:03:06
    
    ---------
    
    Co-authored-by: gelocraft <gelocraft@users.noreply.github.com>

---
+++ src/scripts/content.ts
@@ -0,0 +1,11 @@
+import PullRequest from '../pullrequest'
+import Handlers from '../pullrequest/handlers'
+;(async () => {
+	await PullRequest.getBranchRef(Handlers.getBranchRef)
+		.getWorkflowRuns(Handlers.getWorkflowRuns)
+		.getCoverageWorkflowRun(Handlers.getCoverageWorkflowRun)
+		.getArtifactList(Handlers.getArtifactList)
+		.getCoverageArtifact(Handlers.getCoverageArtifact)
+		.getTestFramework(Handlers.getTestFramework)
+		.highlightCodeCoverage(Handlers.highlightCodeCoverage)
+})()
